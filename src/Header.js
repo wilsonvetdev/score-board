88cdfffd35ecf49efc1263ec446736a6f858852d
wilsonng21 <wilsongch92@gmail.com>
2020-10-06 19:57:34 -0400

    header rendering

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    )
+}
+
+export default Header
