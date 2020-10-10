303a28b217eda8f866f663ba66a29200a0be1514
wilsonng21 <wilsongch92@gmail.com>
2020-10-09 22:45:58 -0400

    covered functional counter to class counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function Counter(props) {
-    return(
-        <div className='counter'>
-            <button className='counter-action decrement'> - </button>
-            <span className='counter-score'> { props.score } </span>
-            <button className='counter-action increment'> + </button>
-        </div>
-    )
+class Counter extends React.Component {
+    render() {
+        return(
+            <div className='counter'>
+                <button className='counter-action decrement'> - </button>
+                <span className='counter-score'> { this.props.score } </span>
+                <button className='counter-action increment'> + </button>
+            </div>
+        )
+    }
 }
 
 export default Counter
