b1d6a5e0f18d3b68752661cb705b1633ca8e6201
wilsonng21 <wilsongch92@gmail.com>
2020-10-10 20:51:12 -0400

    stopwatch setState

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,13 @@ import Stats from './Stats'
 import Stopwatch from './Stopwatch'
 
 function Header(props) {
+
     return (
         <div>
             <header>
                 <Stats totalPlayers={props.totalPlayers} totalScores={props.totalScores} />
                 <h1>{props.title}</h1>
+                <Stopwatch />
             </header>
         </div>
     )
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 class Stopwatch extends React.Component {
+
+    state = {
+        isRunning: false,
+    }
+
+    handleStopwatch = () => {
+        this.setState((prevState) => {
+            return {isRunning: !prevState.isRunning}
+        })
+    }
+
     render() {
         return(
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button> 
+                <button onClick={ this.handleStopwatch } > { this.state.isRunning ? 'Stop' : 'Start' }</button> 
                 <button>Reset</button>
             </div>
         )
