cb9f9d6bca0206a92346ea2d2fff461da910f4dc
wilsonng21 <wilsongch92@gmail.com>
2020-10-11 21:30:22 -0400

    Updates

---
+++ precache-manifest.626bcdf1e02d70a1cbe11a09546a62dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18a82cef72e697f9aab2f2276972867",
+    "url": "/score-board/index.html"
+  },
+  {
+    "revision": "69a37f0330fcdf7ba9f4",
+    "url": "/score-board/static/css/main.d3fc8a07.chunk.css"
+  },
+  {
+    "revision": "05235f9d6f1d01264618",
+    "url": "/score-board/static/js/2.ada3ea10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/score-board/static/js/2.ada3ea10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a37f0330fcdf7ba9f4",
+    "url": "/score-board/static/js/main.d476a23a.chunk.js"
+  },
+  {
+    "revision": "451cc706c38165664d51",
+    "url": "/score-board/static/js/runtime-main.801d5e3e.js"
+  }
+]);
